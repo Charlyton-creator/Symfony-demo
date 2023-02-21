40f7e61d69b0d5d74d3d6493140f6ec8215ce4c0
Youroukou <cyouroukou@gmail.com>
2023-02-21 15:28:11 +0000

    Code à jour

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class IndexController extends AbstractController
 {
@@ -25,5 +30,21 @@ class IndexController extends AbstractController
         $articles = ['Article1', 'Article2', 'Article3'];
         $onearticle = $articles[$id];
         return $this->render('article/detail.html.twig', ['article'=>$onearticle]);
+    }
+    /**
+     * @Route('/article/save')
+     */
+    public function save():Response
+    {
+       $entityManager =  $this->getDoctrine()->getManager();
+
+       $article = new Article();
+       $article->setNom('Article 1');
+       $article->setPrix(1000);
+
+       $entityManager->persist($article);
+       $entityManager->flush();
+
+       return new Response("Article enregistrer avec l'id " . $article->getId());
     }
 }
