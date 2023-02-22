165bd9e395fe8838ff3794b1988abb20b7c9bca2
Youroukou <cyouroukou@gmail.com>
2023-02-22 11:11:57 +0000

    Code mis à jour

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,33 +18,84 @@ class IndexController extends AbstractController
      */
     public function home(){
         //return new Response('<h1> Ma première page </h1>');
-        $articles = ['Article1', 'Article2', 'Article3'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
         return $this->render('article/index.html.twig', ['articles'=>$articles]);
     }
 
     /**
-     * @Route('/article/{id}')
+     * @Route('/article/{id}', name="article_show")
      */
-    public function getOne($id){
+    public function show($id){
         //return new Response('<h1> Ma première page </h1>');
-        $articles = ['Article1', 'Article2', 'Article3'];
-        $onearticle = $articles[$id];
-        return $this->render('article/detail.html.twig', ['article'=>$onearticle]);
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        return $this->render('article/detail.html.twig', ['article'=>$article]);
+    }
+
+    //  /**
+    //  * @Route('/article/save')
+    //  */
+    // public function save():Response
+    // {
+    //    $entityManager =  $this->getDoctrine()->getManager();
+    //    $prices = [1000, 1500, 2000];
+    //    for($i=0; $i<=2;$i++){
+    //     $article = new Article();
+    //     $article->setNom('Article'.$i);
+    //     $article->setPrix($prices[$i]);
+    //     $entityManager->persist($article);
+    //     $entityManager->flush();
+    //    }
+    //    return new Response("Articles enregistrés avec succes ");
+    // }
+    /**
+     * @Route('/article/ajouter')
+     * Method({"GET","POST"})
+     */
+    public function new(Request $request){
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+        ->add('nom', TextType::class)
+        ->add('prix', TextType::class)
+        ->add('save', SubmitType::class, array(
+            'label' => 'Creer')
+        )->getform();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() AND $form->isValid()){
+            $article = $form->getData();
+            $entityManager =  $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('article/new.html.twig', ['form' =>$form->createView()]);
     }
     /**
-     * @Route('/article/save')
+     * @Route('/article/update/{id}', name="article_update")
+     * Method({"GET","PUT"})
      */
-    public function save():Response
-    {
-       $entityManager =  $this->getDoctrine()->getManager();
+    public function update(Request $request, $id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-       $article = new Article();
-       $article->setNom('Article 1');
-       $article->setPrix(1000);
+        $form = $this->createFormBuilder($article)
+        ->add('nom', TextType::class)
+        ->add('prix', TextType::class)
+        ->add('save', SubmitType::class, array(
+            'label' => 'Modifier')
+        )->getform();
 
-       $entityManager->persist($article);
-       $entityManager->flush();
+        $form->handleRequest($request);
+        if($form->isSubmitted() AND $form->isValid()){
+            $article = $form->getData();
+            $entityManager =  $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
 
-       return new Response("Article enregistrer avec l'id " . $article->getId());
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('article/update.html.twig', ['form' =>$form->createView()]);
     }
+
 }
